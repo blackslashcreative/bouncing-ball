33736f132d0e2791924637f8ec6d9e532c6159bb
Black Slash Creative <33047967+blackslashcreative@users.noreply.github.com>
2023-01-15 12:15:15 -0800

    Add files via upload

---
+++ bouncing-ball.js
@@ -0,0 +1,60 @@
+// properties of the html element "ball"
+const ball = document.getElementById("ball");
+const ballWidth = parseInt(ball.style.width);
+const ballHeight = parseInt(ball.style.height);
+// Set two x-axis coordinates to define the bounds of the window
+const Xmin = 0;
+const Xmax = window.innerWidth - ballWidth;
+// two y-axis coordinates
+const Ymin = 0;
+const Ymax = window.innerHeight - ballHeight;
+// variables for direction and position that can be redefined by the function
+let positionX = 0;
+let positionY = 0;
+let velocityX = 7;
+let velocityY = 4;
+
+// write a function that can change the position of the html element "ball"
+function bouncingBall() {
+
+  // move the ball's position 
+  positionX = positionX + velocityX;
+  positionY = positionY + velocityY;
+  ball.style.left = positionX + "px";
+  ball.style.top = positionY + "px";
+
+  // switch directions when it hits a wall 
+  if ( positionX >= Xmax || positionX <= 0 ) {
+    velocityX = -velocityX;
+    var hitWall = true;
+  }
+  if ( positionY >= Ymax || positionY <= 0 ) {
+    velocityY = -velocityY;
+    var hitWall = true;
+  }
+  
+  // change the color of the ball if it hits a wall
+  if (hitWall) {
+    switch(ball.style.backgroundColor) {
+      case "red":
+        ball.style.backgroundColor = "orange";
+        break;
+      case "orange":
+        ball.style.backgroundColor = "green";
+        break;
+      case "green":
+        ball.style.backgroundColor = "blue";
+        break;
+      case "blue":
+        ball.style.backgroundColor = "violet";
+        break;
+      case "violet":
+        ball.style.backgroundColor = "red";
+        break;
+    }
+  }
+
+}
+
+// move the ball  
+setInterval(bouncingBall, 20);
